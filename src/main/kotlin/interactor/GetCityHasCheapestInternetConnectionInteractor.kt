6a224b12399a56d30f71f55f1c295f5c94cf141a
kamel <kikokeko10@gmail.com>
2023-02-12 11:12:14 +0200

    implement execute function and getTheIndex function

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,13 +6,36 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity? {
-        return null
+    fun execute(): CityEntity {
+
+        val list = dataSource.getAllCitiesData()
+            .filter(::excludeNullSalariesAndLowQualityData)
+
+        val index = getTheIndexOfCheapestCityOfInternet(list)
+
+        return list[index]
 
     }
 
     fun getTheIndexOfCheapestCityOfInternet(list: List<CityEntity>): Int {
-        return 0
+        var index = 0
+        var percentage = getThePercentage(
+            list[index].servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!!,
+            list[index].averageMonthlyNetSalaryAfterTax!!
+        )
+
+        list.forEachIndexed { i, it ->
+            val comparePercentage = getThePercentage(
+                it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!!,
+                it.averageMonthlyNetSalaryAfterTax!!
+            )
+            if (comparePercentage <= percentage) {
+                percentage = comparePercentage
+
+                index = i
+            }
+        }
+        return index
     }
 
     fun getThePercentage(n1: Float, n2: Float): Float = n1 / n2
