26b7af349f807620d1d3879085023bb276a4e369
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-02-16 22:19:41 +0300

    update execution function in Interactor class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,13 @@
 import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
 import interactor.CostOfLivingDataSource
-import interactor.GetCityHasCheapestInternetConnectionInteractor
-import interactor.GetHighestSalaryAverageCititesNamesInteractor
 import interactor.getTheAverageSalaryForEachCityInTheCountryInteractor
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
-    val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
-//    println(getHighestSalaryAverageCities.execute(limit = 10))
-//    printSeparationLine()
 
-//    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-//    println(getCityHasCheapestInternetConnectionInteractor.execute())
     val getTheAverageSalaryForEachCityInTheCountry =getTheAverageSalaryForEachCityInTheCountryInteractor(dataSource)
 
 println(getTheAverageSalaryForEachCityInTheCountry.execute("Cuba"))
