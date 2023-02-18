a4d701fc695cb2ae3ff4e8276a0b9d250888fc6c
Oussama Zara <ousamazaara@gmail.com>
2023-02-18 22:15:01 +0100

    update tests according to fake data

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
+
 import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
-import interactor.*
+import interactor.CostOfLivingDataSource
+import interactor.ReorderCitiesByCheapestBananaPricesInteractor
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
+    val list: ReorderCitiesByCheapestBananaPricesInteractor = ReorderCitiesByCheapestBananaPricesInteractor(dataSource)
+    val cities = listOf<String>("Havana", "Jaramana", "Sancti Spiritus", "Madrid", "London")
+//    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+//    println(getCityHasCheapestInternetConnectionInteractor.execute())
 
-    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
-    println(getCityHasCheapestInternetConnectionInteractor.execute())
-
-
+    print(list.reorderTheGivenList("Havana", "Jaramana", "Sancti Spiritus", "Madrid", "London"))
 }
 
--- src/test/kotlin/interactor/ReorderCitiesByCheapestBananaPricesInteractorTest.kt
+++ src/test/kotlin/interactor/ReorderCitiesByCheapestBananaPricesInteractorTest.kt
@@ -1,71 +1,81 @@
 package interactor
 
+import fakedata.FakeDataSource
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
-import kotlin.test.assertNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReorderCitiesByCheapestBananaPricesInteractorTest {
 
-    private lateinit var fakeData: CostOfLivingDataSource
+    private lateinit var fakeData: FakeDataSource
     private lateinit var newListOfCities: ReorderCitiesByCheapestBananaPricesInteractor
 
     @BeforeEach
     fun setUp() {
-        fakeData.getAllCitiesData().take(5)
+        fakeData = FakeDataSource()
         newListOfCities = ReorderCitiesByCheapestBananaPricesInteractor(fakeData)
     }
 
     @Test
     fun should_ReorderCitiesList_When_AllGivenCitiesAreInTheListAndCapitallized() {
         // given a list of capitalized cities are in the database
-        val cities = listOf<String>("Havana", "Jaramana", "Sancti Spiritus", "Madrid", "London")
+        val firstCity = "Madrid"
+        val secondCity = "Havana"
+        val thirdCity = "London"
         // when Reorder cities
-        val reorderedList: List<String> = newListOfCities.reorderTheGivenList(cities.toString())
+        val reorderedList = newListOfCities.reorderTheGivenList(firstCity, secondCity, thirdCity)
         // then find the cheapest banana prices cities
-        assertEquals(cities, reorderedList)
+        assertEquals(listOf("havana", "london", "madrid"), reorderedList)
     }
 
     @Test
     fun should_ReorderCitiesList_When_AGivenCitiesAreNotInTheListAndCapitalized() {
         // given a list of capitalized cities one or more of them are not in the database
-        val cities = listOf<String>("Madrid", "Tokyo", "Alger", "Havana", "Jaramana")
+        val firstCity = "Madrid"
+        val secondCity = "Havana"
+        val thirdCity = "Batna"
         // when Reorder cities
-        val reorderedList: List<String> = newListOfCities.reorderTheGivenList(cities.toString())
+        val reorderedList = newListOfCities.reorderTheGivenList(firstCity, secondCity, thirdCity)
         // then find the cheapest banana prices cities
-        assertEquals(cities, reorderedList)
+        assertEquals(listOf("havana", "madrid"), reorderedList)
     }
 
     @Test
     fun should_ReorderCitiesList_When_AllGivenCitiesAreInTheListAndLowercase() {
         // given a list of cities in lowercase all of them are in the database
-        val cities = listOf<String>("havana", "jaramana", "sancti Spiritus", "madrid", "london")
+        val firstCity = "madrid"
+        val secondCity = "havana"
+        val thirdCity = "london"
         // when Reorder cities
-        val reorderedList: List<String> = newListOfCities.reorderTheGivenList(cities.toString())
+        val reorderedList = newListOfCities.reorderTheGivenList(firstCity, secondCity, thirdCity)
         // then find the cheapest banana prices cities
-        assertEquals(cities, reorderedList)
+        assertEquals(listOf("havana", "london", "madrid"), reorderedList)
     }
 
     @Test
     fun should_ReorderCitiesList_When_AGivenCitiesAreNotInTheListAndLowercase() {
         // given a list of cities in lowercase one or more of them are in the database
-        val cities = listOf<String>("madrid", "tokyo", "alger", "havana", "jaramana")
+        val firstCity = "madrid"
+        val secondCity = "havana"
+        val thirdCity = "batna"
         // when Reorder cities
-        val reorderedList: List<String> = newListOfCities.reorderTheGivenList(cities.toString())
+        val reorderedList = newListOfCities.reorderTheGivenList(firstCity, secondCity, thirdCity)
         // then find the cheapest banana prices cities
-        assertEquals(cities, reorderedList)
+        assertEquals(listOf("havana", "madrid"), reorderedList)
     }
 
     @Test
     fun should_RetournEmptyLis_When_AllGivenCitiesAreNotInTheList() {
         // given a list of all of them are not in the database
-        val cities = listOf<String>("Annaba", "Batna", "Alger", "djelfa", "Blida")
+        val firstCity = "Hammadi"
+        val secondCity = "Rouiba"
+        val thirdCity = "Batna"
         // when Reorder cities
-        val reorderedList: List<String> = newListOfCities.reorderTheGivenList(cities.toString())
+        val reorderedList = newListOfCities.reorderTheGivenList(firstCity, secondCity, thirdCity)
         // then return an empty list
-        assertNull(reorderedList)
+        assertEquals(listOf(), reorderedList)
     }
 
 }
